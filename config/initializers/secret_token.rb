df3604dec3b31672c3d49e25be54486ed575bb75
Miko Bautista <mbautist@andrew.cmu.edu>
2013-02-11 15:48:43 -0500

    initial rails app generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BattleStocks::Application.config.secret_token = 'e2381f0471fee09bea13ded12404d1702737822dde9a831e992dbbad76a1d1cc42c9586d18fdd71b7cdce99ac732e97dd9ccd9de1cb2732ee48d408f3b78a41a'
