57e01bdf66f4942931266862f5300808549c24af
Miko Bautista <mbautist@andrew.cmu.edu>
2013-02-19 14:29:30 -0500

    cleaned stuff

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '237065239764058', '09dcd767c6aa3e4b3ba5c6b29d5ec886',
-  	:scope => 'email,user_birthday,user_location' 
-
-end
