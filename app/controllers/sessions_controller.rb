6fe06d04471f0311dd39443126a64d921dcab5ae
Katarina Shaw <KatarinaShaw@Katarina-Shaws-MacBook.local>
2013-03-26 23:39:17 -0400

    updated the error messages for login, logout, login errors to work with notices

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,15 @@ class SessionsController < ApplicationController
 	  user = User.authenticate(params[:email], params[:password])
 	  if user
 	    session[:user_id] = user.id
-	    redirect_to root_url, :notice => "Logged in!"
+	    redirect_to root_url, :flash => { :notice => "Logged in!" }
 	  else
-	    flash.now.alert = "Invalid email or password"
+	    flash.now[:alert] = "Invalid email or password"
 	    render "new"
 	  end
 	end
 
 	def destroy
 	  session[:user_id] = nil
-	  redirect_to root_url, :notice => "Logged out!"
+    redirect_to root_url, :flash => { :notice => "Logged out!" }
 	end
 end
