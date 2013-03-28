869414d769870c09c03ab8128870f6976d58472b
Miko Bautista <mbautist@andrew.cmu.edu>
2013-03-28 19:55:18 -0400

    with rank

--- app/models/user_game.rb
+++ app/models/user_game.rb
@@ -11,4 +11,10 @@ class UserGame < ActiveRecord::Base
   scope :by_balance, order('balance DESC')
   scope :for_game, lambda { |x| where("game_id = ?", x) }
   scope :for_user, lambda { |x| where("user_id = ?", x) }
+
+  def get_rank
+    hash = Hash[UserGame.for_game(self.game.id).by_balance.map.with_index.to_a]
+    return hash[self] + 1
+  end
+
 end
