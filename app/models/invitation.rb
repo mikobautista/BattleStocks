1522adca2af2d0b52da78431a053f1c75c58bed6
sennap <sennaparsa@gmail.com>
2013-03-28 16:21:28 -0300

    Update invitation.rb
    email validation

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -6,5 +6,10 @@ class Invitation < ActiveRecord::Base
   
   # Validations
   validates_presence_of :game_id
+    
+  # email must be unique and in proper format
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
 
 end
