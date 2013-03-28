fb86b14acc67cfc9e0db8f16a7a34ba4c4d0775b
sennap <sennaparsa@gmail.com>
2013-03-28 20:07:58 -0300

    Update purchased_stock_test.rb

--- test/unit/purchased_stock_test.rb
+++ test/unit/purchased_stock_test.rb
@@ -14,7 +14,7 @@ class PurchasedStockTest < ActiveSupport::TestCase
        :end_date => 10.days.from_now.to_date, :budget => 10)
        @usergame1 = FactoryGirl.create(:user_game, :user_id => @alex, :game_id => @game1)
        @goog = FactoryGirl.create(:purchased_stock, :user_game => @usergame1, 
-        :stock_code => 23, :total_quantity => 40, money_spent => 5000, :money_earned => 509000,
+        :stock_code => "goog", :total_quantity => 40, money_spent => 5000, :money_earned => 509000,
         :value_in_stocks => 80)
        @transaction1 = FactoryGirl.create(:transaction, :purchased_stock => @goog, :date => Time.now.to_date, :qty => 40, :value_per_stock => 45000, :is_buy => true)
      end
@@ -34,7 +34,7 @@ class PurchasedStockTest < ActiveSupport::TestCase
        assert_equal "test@gmail.com", @alex.email
        assert_equal "testuser1", @alex.username
        assert_equal "test gameeee", @game1.name
-       assert_equal 23, @goog.stock_code
+       assert_equal "goog", @goog.stock_code
        assert_equal 40, @transaction1.qty
      end
 
