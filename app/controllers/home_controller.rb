f061be5ecff9c72cd56a372dea9cab9b2318b234
Katarina Shaw <KatarinaShaw@Katarina-Shaws-MacBook.local>
2013-03-30 23:24:54 -0400

    added usergame info to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,9 @@
 class HomeController < ApplicationController
   def index
   	if logged_in?
-  		@current_games = Game.current.for_user(current_user).ending_soonest
-  		@upcoming_games = Game.upcoming.for_user(current_user).starting_soonest
-  		@past_games = Game.past.for_user(current_user).most_recent
-
   		@current_user_games = UserGame.current.for_user(current_user).ending_soonest
+  		@upcoming_user_games = UserGame.upcoming.for_user(current_user).starting_soonest
+  		@past_user_games = UserGame.past.for_user(current_user).most_recent
 
   		@owned_stock = PurchasedStock.for_user(current_user)
   	end
--- app/models/user_game.rb
+++ app/models/user_game.rb
@@ -12,7 +12,11 @@ class UserGame < ActiveRecord::Base
   scope :for_game, lambda { |x| where("game_id = ?", x) }
   scope :for_user, lambda { |x| where("user_id = ?", x) }
   scope :current, joins(:game).where('start_date <= ?', Time.now).where('end_date > ?', Time.now).where('is_terminated = ?', false)
+  scope :upcoming, joins(:game).where('start_date > ?', Time.now).where('is_terminated = ?', false)
+  scope :past, joins(:game).where('end_date <= ?', Time.now)
   scope :ending_soonest, joins(:game).order('end_date, start_date')
+  scope :starting_soonest, joins(:game).order('start_date, end_date')
+  scope :most_recent, joins(:game).order('end_date DESC, start_date DESC')
 
   def get_rank
     hash = Hash[UserGame.for_game(self.game.id).by_balance.map.with_index.to_a]
