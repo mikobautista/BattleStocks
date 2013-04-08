d1e3903f1fc68b97a1349b136396127f42dee47a
sennap <sennaparsa@gmail.com>
2013-04-08 20:42:34 -0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
   
   validates_presence_of :username
   validates_uniqueness_of :username, :email
-  validates_format_of :username, :with => /^[-\w\._@]+$/i, :allow_blank => true, :message => "should only contain letters, numbers, or .-_@"
+  validates_format_of :username, :with => /^[-\w\._@]+$/i, :message => "should only contain letters, numbers, or .-_@"
   validates_presence_of :password, :on => :create
 
   # email must be unique and in proper format
