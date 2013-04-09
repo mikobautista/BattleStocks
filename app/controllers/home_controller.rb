a691a3ee90193cf250febc1991e49ebafddd4b7d
Katarina Shaw <KatarinaShaw@Katarina-Shaws-MacBook.local>
2013-04-09 17:39:10 -0400

    added search stock functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,6 +14,6 @@ class HomeController < ApplicationController
   def search 
     # allows for the admin to search from their dashboard
     @query = params[:query]
-    @stock_value = PurchasedStock.search(@query)
+    @stock_value = PurchasedStock.search("#{params[:query]}")
   end
 end
