9f8c47cc2339d691fc3e75acc3dc9547e546a1b6
sennap <sennaparsa@gmail.com>
2013-04-10 00:43:36 -0400

    fixing the purchased stock test, user game test, and transaction test

--- test/unit/user_game_test.rb
+++ test/unit/user_game_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserGameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Relationships
+  # -----------------------------
+  should have_many :purchased_stocks
+  should belong_to :user
+  should belong_to :game
+
+  # Validations
+  # -----------------------------
+  should validate_presence_of :user_id
+  should validate_presence_of :game_id
 end
