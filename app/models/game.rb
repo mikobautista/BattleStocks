84b504bb36ddcba7e38dd4a78506bf042f78ceaf
Emily Tsai <cetsai@andrew.cmu.edu>
2013-04-23 14:37:21 -0400

    form message bug fixed

--- app/models/game.rb
+++ app/models/game.rb
@@ -11,8 +11,8 @@ class Game < ActiveRecord::Base
   # -----------------------------
   validates_numericality_of :budget, :greater_than => 0
   validates_format_of :name, :with => /.+/, :message => "name cannot be blank"
-  validates_date :start_date, :on_or_after => lambda { Date.current }, :message => "start date must start tomorrow onwards"
-  validates_date :end_date, :on_or_after => :start_date, :message => "end date must be on or after start date"
+  validates_date :start_date, :on_or_after => lambda { Date.current }, :on_or_after_message => "start date must start tomorrow onwards"
+  validates_date :end_date, :on_or_after => :start_date, :on_or_after_message => "end date must be on or after start date"
   validates_presence_of :budget
   validates_presence_of :end_date
   validates_presence_of :start_date
